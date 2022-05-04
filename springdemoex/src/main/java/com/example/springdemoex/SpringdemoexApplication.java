6baf8b5f3d43f4100175fb7b750a35ae8fd7c71c
M1082731 <M1082731@MT-HHJXYY2>
2022-05-04 16:03:30 +0530

    commit

---
+++ springdemoex/src/main/java/com/example/springdemoex/SpringdemoexApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemoex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoexApplication.class, args);
+	}
+
+}
---
+++ springdemoex/src/test/java/com/example/springdemoex/SpringdemoexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemoex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
